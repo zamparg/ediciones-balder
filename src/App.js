47fc4640e55257be30930424042708a98eeb5376
zamparg <zamparg@hotmail.com>
2022-07-04 22:39:01 -0300

    NavBar mediante components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenidos!
-        </p>
-        <a
-          className="App-link"
-          href="https://arte-escuela.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Arte-Escuela
-        </a>
-      </header>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavBar = () =>{
+    return(
+        <>
+         <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                <img src={require("../imgs/logoAE.svg").default} alt="" width="30" height="24" className="d-inline-block align-text-top"/>
+                Cursos de Arte
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"/>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Dibujo y pintura</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Música</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Diseño Gráfico</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Craft</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>   
+        </>
+    );
+}
+
+export default NavBar;
